e2376fdd73e9db97a66f625e8c4c9a4fbccf0419
anton <maladoy+1@gmail.com>
2016-06-10 20:23:20 -0700

    add serverscope.io comment :)

--- serverscope.py
+++ serverscope.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+################################################################################
+#                       Serverscope.io benchmark tool                          #
+################################################################################
+
 import os, sys, stat, platform
 devnull = open(os.devnull, 'w')
 
