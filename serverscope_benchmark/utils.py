cdae774ad6d21c74616ac69813ca022b8bdb9581
Mikhail Campos Guadamuz <plageat90@gmail.com>
2021-03-30 12:31:57 +0300

    some code refactoring to fit python3.5+

--- serverscope_benchmark/utils.py
+++ serverscope_benchmark/utils.py
@@ -3,7 +3,6 @@
 import os
 import sys
 import subprocess
-import signal
 import locale
 import urllib
 import requests
@@ -24,40 +23,22 @@ class Color:
 c = Color
 
 
-# from http://hg.python.org/cpython/rev/768722b2ae0a/
-def restore_signals():
-    signals = ('SIGPIPE', 'SIGXFZ', 'SIGXFSZ')
-    for sig in signals:
-        if hasattr(signal, sig):
-            signal.signal(getattr(signal, sig), signal.SIG_DFL)
-
-
 def run_and_print(command, cwd=None):
-    p = subprocess.Popen(command,
-                         stdout=subprocess.PIPE,
-                         stderr=subprocess.STDOUT,
-                         bufsize=-1,
-                         cwd=cwd,
-                         preexec_fn=restore_signals,
-                         universal_newlines=True)
-    chunks = []
-    encoding = locale.getdefaultlocale()[1] or 'ascii'
 
-    try:
-        while True:
+    chunks = []
+
+    with subprocess.Popen(command,
+                          stdout=subprocess.PIPE,
+                          stderr=subprocess.STDOUT,
+                          cwd=cwd,
+                          universal_newlines=True) as p:
+        chunk = p.stdout.readline()
+        while chunk:
+            sys.stdout.write(chunk)
+            chunks.append(chunk)
+            sys.stdout.flush()
             chunk = p.stdout.readline()
-            if chunk != '':
-                try:
-                    getattr(sys.stdout, 'buffer', sys.stdout).write(chunk.encode(encoding))
-                    sys.stdout.flush()
-                except UnicodeDecodeError:
-                    pass
-                chunks.append(chunk)
-            else:
-                break
-    finally:
-        p.stdout.close()
-    p.wait()
+
     return ''.join(chunks)
 
 
